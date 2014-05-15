298f344094e90d8bde22f04da188a1149f420c28
kb3dow <r.rajamani@gmail.com>
2014-05-15 16:29:21 -0400

    Update instdotfiles.sh

--- instdotfiles.sh
+++ instdotfiles.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc zshrc oh-my-zsh private Xresources"    # list of files/folders to symlink in homedir
+files="bashrc vimrc conkyrc zshrc oh-my-zsh private Xresources"    # list of files/folders to symlink in homedir
 
 ##########
 
